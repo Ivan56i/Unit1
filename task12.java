4e9562367cad6a282926713ea2183f70f0bf354f
Ivan56i <84318070+Ivan56i@users.noreply.github.com>
2021-05-25 19:44:28 +0300

    Update task12.java

--- task12.java
+++ task12.java
@@ -1,6 +1,5 @@
   public static int getInt() {    
  System.out.println("Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера. ");
-        
         int sum = 0;
         int number = 0;
     String entry = "";
@@ -10,13 +9,14 @@
         entry = keyboard.next();
         System.out.println("Некорректный ввод!" + "Введите число");
     }
+    
     number = Integer.parseInt(entry);
-    System.out.print(number);
-    return number;
-        
-        for (int i = 1; i <= number; i++) {
+    for (int i = 1; i <= number; i++) {
             sum += i ;
         }
-
-        System.out.println("Сумма введенных чисел " + number + "равна: " + sum);
+    System.out.print(number);
+    System.out.println("Сумма введенных чисел " + number + "равна: " + sum);
+    
+    return number;
+       
         }
