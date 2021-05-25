8c960a6f6d510d9544a2bed3c8465bac48effdaf
Ivan56i <84318070+Ivan56i@users.noreply.github.com>
2021-05-25 21:06:01 +0300

    Update task19.java

--- task19.java
+++ task19.java
@@ -7,23 +7,17 @@ public class MyClass {
         int b = 542;
         int[] arr = new int[10];
         int i = 0;
-        while(a > 0 & b> 0){
+        while(a > 0 & b> 0 ){
             arr[i] = a % 10;
             a = a / 10;
-            i++;
+             i++;
             arr[i] = b % 10;
             b = b / 10;
-            i++;
-        }
+             i++;
+        }  
         for(int j = 0; j < arr.length; j++){
             System.out.print(" " + arr[j]);
         }
-        while(b > 0){
-            arr[i] = b % 10;
-            b = b / 10;
-            i++;
-        }
-       
         
     }
 }
